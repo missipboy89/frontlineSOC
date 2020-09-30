dc353bb2bc5c498361486013689fd370723a8fb8
play.js <git@gituser.com>
2020-09-30 15:15:01 -0500

    initial setup

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
-import Vue from 'vue'
-import App from './App'
-
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App';
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    render: h => h(App)
+}).$mount('#app');
